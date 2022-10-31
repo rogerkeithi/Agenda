f28687fe6005e843e2001257db2e71b26ed586ed
Roger Keithi Nojiri da Silva <nojiri.keithi@aluno.ifsp.edu.br>
2022-10-30 23:06:25 -0300

    upload

---
+++ src/main/java/com/roger/agenda/Agenda.java
@@ -0,0 +1,32 @@
+package com.roger.agenda;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Agenda {
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+           // handle exception
+        }
+        catch (ClassNotFoundException e) {
+           // handle exception
+        }
+        catch (InstantiationException e) {
+           // handle exception
+        }
+        catch (IllegalAccessException e) {
+           // handle exception
+        }
+        
+        
+        Tela tela = new Tela();
+        tela.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        tela.setVisible(true);
+    }
+}
