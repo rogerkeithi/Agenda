0a4319f482cc3c79d2dc11e7bf9895a13498333f
Roger Keithi Nojiri da Silva <nojiri.keithi@aluno.ifsp.edu.br>
2022-11-02 13:36:12 -0300

    Upload backup

---
+++ src/main/java/com/roger/agenda/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package com.roger.agenda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection getConnection() throws SQLException{
+        
+        try{
+            
+            Class.forName("com.mysql.jdbc.Driver");
+
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/agenda?useSSL=false", "root", "");
+            
+        } catch(ClassNotFoundException ex){
+            
+            throw new SQLException(ex.getMessage());
+        }
+    }
+}
