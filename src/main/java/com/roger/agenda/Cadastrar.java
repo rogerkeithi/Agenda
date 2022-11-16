e37fd88b97b41693ea6ac5de5d6b7b0f8d49c328
Roger Keithi Nojiri da Silva <nojiri.keithi@aluno.ifsp.edu.br>
2022-11-16 02:36:39 -0300

    a

--- src/main/java/com/roger/agenda/Cadastrar.java
+++ src/main/java/com/roger/agenda/Cadastrar.java
@@ -10,7 +10,7 @@ import javax.swing.JOptionPane;
 public class Cadastrar {
     private static ArrayList<Pessoa> pessoa;
     private static ArrayList<Login> login;
-    private static String FKUsuario;
+    private static String ID_USUARIO_FK;
 
     public void setPessoa(ArrayList<Pessoa> newPessoa){
         this.pessoa = newPessoa;
@@ -92,11 +92,62 @@ public class Cadastrar {
             con.commit();
 
             while (rs.next()) {
-                FKUsuario = rs.getString("ID_USUARIO");
+                ID_USUARIO_FK = rs.getString("ID_USUARIO");
             }
-
+            System.out.println(ID_USUARIO_FK);
             ps.close();
 
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+    
+    public void selectID_PESSOA(){
+        Connection con;
+        
+        String query = "SELECT ID_PESSOA FROM tb_pessoa WHERE USUARIO = ?";
+        PreparedStatement ps;
+        ResultSet rs;
+
+        try {
+            ConnectionFactory cf = new ConnectionFactory();
+            con = cf.getConnection();
+            con.setAutoCommit(false);
+            ps = con.prepareStatement(query);
+            ps.setString(1, this.login.get(0).getUsuario());
+            rs = ps.executeQuery();
+            con.commit();
+
+            while (rs.next()) {
+                ID_USUARIO_FK = rs.getString("ID_USUARIO");
+            }
+            System.out.println(ID_USUARIO_FK);
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+    
+    public void atualizarID_USUARIO(){
+        Connection con;
+        
+        String query = "UPDATE tb_pessoa SET ID_USUARIO = ? WHERE ID_PESSOA = LAST_INSERT_ID()";
+
+        PreparedStatement ps;
+
+        try {
+            con = ConnectionFactory.getConnection();
+            con.setAutoCommit(false);
+
+            ps = con.prepareStatement(query);
+            ps.setInt(1, Integer.parseInt(ID_USUARIO_FK));
+            ps.execute();
+            con.commit();
+            ps.close();
+
+            JOptionPane.showMessageDialog(null, "Alteração feita com sucesso!");
+            
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
         }
